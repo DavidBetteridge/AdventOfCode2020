fc15db6a34eaf79debe41e7f177f5f87ecc6c672
David Betteridge <david.betteridge@proactis.com>
2020-12-03 08:27:29 +0000

    Day 3 - Part 2

--- Day3/day3.py
+++ Day3/day3.py
@@ -1,21 +1,21 @@
 lines = open('Day3/day3.txt').read().splitlines()
 
-currentX = 0
-currentY = 0
-stepX = 3
-stepY = 1
-treesFound = 0
-forestHeight = len(lines)
-forestWidth = len(lines[0])
+def countTrees(stepX, stepY):
+    currentX = 0
+    currentY = 0
+    treesFound = 0
+    forestHeight = len(lines)
+    forestWidth = len(lines[0])
 
-while (currentY + 1 < forestHeight):
-    currentX = (currentX + stepX) % forestWidth
-    currentY = currentY + stepY
+    while (currentY + 1 < forestHeight):
+        currentX = (currentX + stepX) % forestWidth
+        currentY = currentY + stepY
 
-    symbol = lines[currentY][currentX]
+        symbol = lines[currentY][currentX]
 
-    if (symbol == '#'):
-        treesFound = treesFound + 1
+        if (symbol == '#'):
+            treesFound = treesFound + 1
 
+    return treesFound
 
-print(treesFound)
+print(countTrees(1,1) * countTrees(3,1) * countTrees(5,1) * countTrees(7,1) * countTrees(1,2))
