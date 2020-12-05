390eb604403134402f4b8617b932b67fa4ec076d
David Betteridge <david.betteridge@proactis.com>
2020-12-05 10:07:53 +0000

    Code golfed

--- Day5/day5.py
+++ Day5/day5.py
@@ -1,18 +1,3 @@
-def calculate_seatId(line):
-    rowBinary = line[:7].replace('B','1').replace('F','0')
-    seatBinary = line[7:].replace('R','1').replace('L','0')
-    row = int(rowBinary, 2)
-    seat = int(seatBinary, 2)
-    return (row * 8) + seat
-
-boardingPasses = open('Day5/day5.txt').read().splitlines()
-seatIds = list(map(calculate_seatId, boardingPasses))
-highestSeatId = max(seatIds)
-print(highestSeatId)
-
-for row in range(int('0000001',2), int('1111110',2)):
-    for column in range(0,7):
-        seatId = (row * 8) + column
-        if not seatId in seatIds:
-            if (seatId - 1 in seatIds) and (seatId + 1 in seatIds):
-                print(seatId)
+seatIds = list(map(lambda line: int(line.replace('B','1').replace('F','0').replace('R','1').replace('L','0'), 2), open('Day5/day5.txt').read().splitlines()))
+print(max(seatIds))
+print(next((seatId for seatId in range(0, 1024) if (not seatId in seatIds) and (seatId - 1 in seatIds) and (seatId + 1 in seatIds)), None))
