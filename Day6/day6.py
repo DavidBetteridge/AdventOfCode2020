59bfead35386942b51c0ed78007e39db6131e1fb
David Betteridge <david.betteridge@proactis.com>
2020-12-06 22:22:25 +0000

    Code golfed!

--- Day6/day6.py
+++ Day6/day6.py
@@ -3,17 +3,15 @@ from functools import reduce
 groups = open('Day6/day6.txt').read().split('\n\n')
 
 def solve(fn):
-    def solve_group(group):
-        people = map(set, group.split('\n'))
-        return len(reduce(lambda p1, p2 : fn(p1, p2), people))
+    return sum(map(lambda group: len(reduce(lambda p1, p2 : fn(p1, p2), map(set, group.split('\n')))), groups))
 
-    return sum(map(solve_group, groups))
+print(f"Part 1 = {solve(lambda x, y: x | y)}")
+print(f"Part 2 = {solve(lambda x, y: x & y)}")
 
-def part1():
-    print(solve(lambda x, y: x | y))
+# part1()     #6585
+# part2()     #3276
 
-def part2():
-    print(solve(lambda x, y: x & y))
-
-part1()     #6585
-part2()     #3276
+# Short version!
+# from functools import reduce
+# print(sum(map(lambda group: len(reduce(lambda p1, p2 : p1 | p2, map(set, group.split('\n')))), open('Day6/day6.txt').read().split('\n\n'))))
+# print(sum(map(lambda group: len(reduce(lambda p1, p2 : p1 & p2, map(set, group.split('\n')))), open('Day6/day6.txt').read().split('\n\n'))))
