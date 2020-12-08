10a1d8d30f4d6063abe774aecf98ea49fcd70afc
David Betteridge <david.betteridge@proactis.com>
2020-12-08 08:26:49 +0000

    Day 8 - Part 2

--- Day8/day8.py
+++ Day8/day8.py
@@ -1,25 +1,50 @@
-instructionPointer = 0
-accumulator = 0
-seenCommands = []
 commands = []
 for command in open('Day8/day8.txt').read().splitlines():
     instruction, operand = command.split(' ')    
     commands.append( (instruction, int(operand)) )
 
+def run_program():
+    instructionPointer = 0
+    accumulator = 0
+    seenCommands = []
 
-while not instructionPointer in seenCommands:
-    seenCommands.append(instructionPointer)
-    instruction, operand = commands[instructionPointer]
+    while (not instructionPointer in seenCommands) and instructionPointer < len(commands):
+        seenCommands.append(instructionPointer)
+        instruction, operand = commands[instructionPointer]
 
-    if instruction == 'nop':
-        instructionPointer += 1
+        if instruction == 'nop':
+            instructionPointer += 1
 
-    if instruction == 'acc':
-        accumulator += operand
-        instructionPointer += 1
+        if instruction == 'acc':
+            accumulator += operand
+            instructionPointer += 1
 
-    if instruction == 'jmp':
-        instructionPointer += operand
+        if instruction == 'jmp':
+            instructionPointer += operand
 
+    infiniteLoop = instructionPointer < len(commands)
+    return infiniteLoop, accumulator
 
-print(accumulator)
+def part_one():
+    infiniteLoop, accumulator = run_program()
+    print(accumulator)  #1810
+
+def part_two():
+    
+    for commandNumber in range(0, len(commands)):
+
+        originalInstruction, operand = commands[commandNumber]
+        if (originalInstruction == 'nop'):
+            commands[commandNumber] = ('jmp', operand)
+
+        if (originalInstruction == 'jmp'):
+            commands[commandNumber] = ('nop', operand)
+
+        if (originalInstruction != 'acc'):
+            infiniteLoop, accumulator = run_program()
+            if not infiniteLoop:
+                print(accumulator)
+                break
+            commands[commandNumber] = (originalInstruction, operand)
+
+part_two()        
