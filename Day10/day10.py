efd45b7e3f62c22530c0fb604d430aa2cad4d000
David Betteridge <david.betteridge@proactis.com>
2020-12-10 09:15:51 +0000

    Day 10 - Part 2

--- Day10/day10.py
+++ Day10/day10.py
@@ -1,23 +1,39 @@
 lines = sorted(map(int,open('Day10/day10.txt').read().splitlines()))
 
-oneCount=0
-threeCount=1
+def part_one():
+    oneCount=0
+    threeCount=1
 
-if lines[0] == 1:
-    oneCount += 1
-
-if lines[0] == 3:
-    threeCount += 1
-
-for i in range(0, len(lines) - 1):
-    if lines[i + 1] - lines[i] == 1:
+    if lines[0] == 1:
         oneCount += 1
-    if lines[i + 1] - lines[i] == 3:
+
+    if lines[0] == 3:
         threeCount += 1
 
-print(oneCount)        
-print(threeCount)        
+    for i in range(0, len(lines) - 1):
+        if lines[i + 1] - lines[i] == 1:
+            oneCount += 1
+        if lines[i + 1] - lines[i] == 3:
+            threeCount += 1
 
-print(oneCount * threeCount)
+    return oneCount * threeCount
+    #2170
+    #zip might be better
 
-#2170
+def part_two():
+    lines.insert(0,0)
+    lines.append(lines[len(lines)-1] + 3)
+
+    numberOfRoots = {}
+    numberOfRoots[len(lines)-1] = 1
+
+    for i in range(len(lines)-2, -1, -1):
+        numberOfRoots[i] = 0
+        index = i+1
+        while index < len(lines) and (lines[index] - lines[i]) <= 3:
+            numberOfRoots[i] += numberOfRoots[index]
+            index = index + 1
+
+    return numberOfRoots[0]
+
+print(part_two())    
