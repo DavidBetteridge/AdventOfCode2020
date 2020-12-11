8b3b2f02f4c3a1c38d98d947d04985c4d73ecf6c
David Betteridge <david.betteridge@proactis.com>
2020-12-11 09:30:13 +0000

    Day 11 Part 2

--- Day11/day11.py
+++ Day11/day11.py
@@ -2,7 +2,7 @@ grid = open('Day11/day11.txt').read().splitlines()
 rows = len(grid)
 columns = len(grid[0])
 
-def neighbours(x, y, grid):
+def adjacent_seats(x, y, grid):
     #  abc
     #  dXe
     #  fgh 
@@ -41,18 +41,105 @@ def neighbours(x, y, grid):
     return result
 
 
-def mutate_grid(currentGrid):
+def visible_seats(x, y, grid):
+    currentX  = x
+    currentY  = y
+
+    #  abc
+    #  dXe
+    #  fgh 
+    result = []
+    #a
+    while x > 0 and y > 0:
+        x -= 1
+        y -= 1
+        if grid[y][x] != '.':
+            result.append(grid[y][x])
+            break
+    x = currentX
+    y = currentY
+
+    #b
+    while y > 0:
+        y -= 1        
+        if grid[y][x] != '.':
+            result.append(grid[y][x])
+            break
+    x = currentX
+    y = currentY
+
+    #c
+    while x + 1 < columns and y > 0:
+        x += 1
+        y -= 1        
+        if grid[y][x] != '.':
+            result.append(grid[y][x])
+            break
+    x = currentX
+    y = currentY
+
+    #d
+    while x > 0:
+        x -= 1
+        if grid[y][x] != '.':
+            result.append(grid[y][x])
+            break
+    x = currentX
+    y = currentY
+
+    #e
+    while x + 1 < columns:
+        x += 1
+        if grid[y][x] != '.':
+            result.append(grid[y][x])
+            break
+    x = currentX
+    y = currentY
+
+    #f
+    while x > 0 and y + 1 < rows:
+        x -= 1
+        y += 1        
+        if grid[y][x] != '.':
+            result.append(grid[y][x])
+            break
+    x = currentX
+    y = currentY
+
+    #g
+    while y + 1 < rows:
+        y += 1        
+        if grid[y][x] != '.':
+            result.append(grid[y][x])
+            break
+    x = currentX
+    y = currentY
+
+    #h
+    while x + 1 < columns and y + 1 < rows:
+        x += 1
+        y += 1        
+        if grid[y][x] != '.':
+            result.append(grid[y][x])
+            break
+    x = currentX
+    y = currentY
+
+    return result
+
+
+def mutate_grid(seatsFunction, minOccupiedSeatCount, currentGrid):
     newGrid = []
     changed = False
 
     for r in range(0, rows):
         row = ""
         for c in range(0, columns):
-            n = neighbours(c,r,currentGrid)
+            n = seatsFunction(c,r,currentGrid)
             if n.count('#') == 0 and currentGrid[r][c] == 'L':
                 row += '#'
                 changed = True
-            elif currentGrid[r][c] == '#' and n.count('#') >= 4:
+            elif currentGrid[r][c] == '#' and n.count('#') >= minOccupiedSeatCount:
                 row += 'L'
                 changed = True
             else:
@@ -60,10 +147,10 @@ def mutate_grid(currentGrid):
         newGrid.append(row)
     return (newGrid, changed)
 
-def part_one():
+def part_one(grid):
     changed = True
     while changed:
-        grid, changed = mutate_grid(grid)        
+        grid, changed = mutate_grid(adjacent_seats, 4, grid)        
 
     count = 0
     for row in grid:
@@ -71,4 +158,23 @@ def part_one():
 
     return count
 
+def part_two(grid):
+    changed = True
+
+    while changed:
+        grid, changed = mutate_grid(visible_seats, 5, grid)        
+
+        count = 0
+        for row in grid:
+            count += row.count('#')        
+        print(count)
+
+
+    count = 0
+    for row in grid:
+        count += row.count('#')
+
+    return count
+
+print(part_two(grid))
 #2329
