12509d075175049c26ad7b14eab6a1b8616b178b
David Betteridge <david.betteridge@proactis.com>
2020-12-17 21:57:47 +0000

    Combined parts 1 and 2

--- Day17/day17.py
+++ Day17/day17.py
@@ -2,87 +2,58 @@ from copy import deepcopy
 
 lines = open('Day17/day17.txt').read().splitlines()
 
-grid = {}
-for x in range(len(lines[0])):
-    for y in range(len(lines)):
-        if lines[y][x] == "#":
-            grid[(0,x,y,0)] = True
+def load_grid():
+    grid = {}
+    for x in range(len(lines[0])):
+        for y in range(len(lines)):
+            if lines[y][x] == "#":
+                grid[(0,x,y,0)] = True
+    return grid
 
-def count_active_neighbours(grid, w, x, y, z):
+def is_alive(grid, w, x, y, z):        
+    return (w,x,y,z) in grid and grid[(w,x,y,z)] == True
+
+def count_active_neighbours(grid, w, x, y, z, wOffsetRange):
     count = 0
-    for wOffset in [-1, 0, 1]:
+    for wOffset in wOffsetRange:
         for xOffset in [-1, 0, 1]:
             for yOffset in [-1, 0, 1]:
                 for zOffset in [-1, 0, 1]:
                     if wOffset != 0 or xOffset != 0 or yOffset != 0 or zOffset != 0:
-                        if (w+wOffset,x+xOffset,y+yOffset,z+zOffset) in grid and grid[(w+wOffset,x+xOffset,y+yOffset,z+zOffset)] == True:
+                        if is_alive(grid, w+wOffset, x+xOffset, y+yOffset, z+zOffset):
                             count += 1
     return count                                                
 
-# def display(grid, x, y, z):
-#     if is_alive(grid, x, y, z):
-#         return "#"
-#     else:
-#         return "."
+def solve(wOffsetRange):
+    grid = load_grid()
+    for _ in range(6):
+        previous = deepcopy(grid)
+        minW = min(grid, key = lambda t: t[0])[0]
+        minX = min(grid, key = lambda t: t[1])[1]
+        minY = min(grid, key = lambda t: t[2])[2]
+        minZ = min(grid, key = lambda t: t[3])[3]
+        maxW = max(grid, key = lambda t: t[0])[0]
+        maxX = max(grid, key = lambda t: t[1])[1]
+        maxY = max(grid, key = lambda t: t[2])[2]
+        maxZ = max(grid, key = lambda t: t[3])[3]
 
-# def displayGrid(grid):
-#     minX = min(grid, key = lambda t: t[0])[0]
-#     minY = min(grid, key = lambda t: t[1])[1]
-#     minZ = min(grid, key = lambda t: t[2])[2]
-#     maxX = max(grid, key = lambda t: t[0])[0]
-#     maxY = max(grid, key = lambda t: t[1])[1]
-#     maxZ = max(grid, key = lambda t: t[2])[2]
-        
-#     for z in range(minZ, maxZ+1):
-#         print(f"z {z}")
-#         for y in range(minY, maxY+1):
-#             row = ""
-#             for x in range(minX, maxX+1):
-#                 row = row + display(grid, x, y, z)                           
-#             print(row)                
-#     print("")      
+        for w in range(minW - 1, maxW + 2):
+            for x in range(minX - 1, maxX + 2):
+                for y in range(minY - 1, maxY + 2):
+                    for z in range(minZ - 1, maxZ + 2):
+                        active_neighbours = count_active_neighbours(previous, w, x, y, z, wOffsetRange)
 
-def is_alive(grid, w, x, y, z):        
-    return (w, x,y,z) in grid and grid[(w, x,y,z)] == True
+                        if is_alive(previous, w, x, y, z):
+                            if active_neighbours != 2 and active_neighbours != 3:
+                                grid[(w,x,y,z)] = False
 
-
-
-
-for cycle in range(6):
-    previous = deepcopy(grid)
-    minW = min(grid, key = lambda t: t[0])[0]
-    minX = min(grid, key = lambda t: t[1])[1]
-    minY = min(grid, key = lambda t: t[2])[2]
-    minZ = min(grid, key = lambda t: t[3])[3]
-    maxW = max(grid, key = lambda t: t[0])[0]
-    maxX = max(grid, key = lambda t: t[1])[1]
-    maxY = max(grid, key = lambda t: t[2])[2]
-    maxZ = max(grid, key = lambda t: t[3])[3]
-
-    for w in range(minW - 1, maxW + 2):
-        for x in range(minX - 1, maxX + 2):
-            for y in range(minY - 1, maxY + 2):
-                for z in range(minZ - 1, maxZ + 2):
-                    active_neighbours = count_active_neighbours(previous, w, x, y, z)
-
-                    if is_alive(previous, w, x, y, z):
-                        if active_neighbours == 2 or active_neighbours == 3:
-                            grid[(w,x,y,z)] = True
                         else:
-                            grid[(w,x,y,z)] = False
+                            if active_neighbours == 3:
+                                grid[(w,x,y,z)] = True
 
-                    else:
-                        if active_neighbours == 3:
-                            grid[(w,x,y,z)] = True
+    print(sum([grid[k] for k in grid]))
 
-    count = 0
-    for k in grid:
-        if grid[k]:
-            count += 1
+solve([0])
+solve([-1,0,1])
 
-    print(f"Cycle: {cycle + 1}")
-    print(count)
-  #  displayGrid(grid)              
-
-#117 too low
-#406 too high
+#1868
