86ca4518993c1d27c210f4cca3dda7b645b46bdd
David Betteridge <david.betteridge@proactis.com>
2020-12-22 08:30:42 +0000

    Day 22 - Part 1

---
+++ Day22/day22.py
@@ -0,0 +1,50 @@
+import re
+
+def read_file():
+    pack1 = []
+    pack2 = []
+    pack = []
+    lines = open('Day22/day22.txt').read().splitlines()
+    for line in lines:
+        if line == "Player 1:":
+            pack = pack1
+        elif line == "Player 2:":
+            pack = pack2
+        elif line == "":            
+            pass
+        else:
+            pack.append(int(line))
+    return (pack1,pack2)
+
+def score_pack(pack):
+    return sum([card * (len(pack) - i) for i, card in enumerate(pack)])
+
+
+def part_one():
+    pack1, pack2 = read_file()
+    while len(pack1) > 0 and len(pack2) > 0:
+        top1 = pack1[0]
+        pack1 = pack1[1:]
+        top2 = pack2[0]
+        pack2 = pack2[1:]
+
+        if top1 > top2:
+            pack1.append(top1)
+            pack1.append(top2)
+        else:
+            pack2.append(top2)
+            pack2.append(top1)            
+
+    if len(pack1) > 0:
+        return score_pack(pack1)
+    else:
+        return score_pack(pack2)            
+
+def part_two():
+    pass
+
+
+
+
+
+print(part_one())
