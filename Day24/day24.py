4e252bc614d73235d94f9ea74cbc741aed95694d
David Betteridge <david.betteridge@proactis.com>
2020-12-27 23:17:16 +0000

    Speed up Day 24

--- Day24/day24.py
+++ Day24/day24.py
@@ -1,3 +1,17 @@
+import time
+import cProfile
+
+def time_it(what):
+    def decorator(func):
+        def wrapper():
+            tic = time.perf_counter()
+            result = func()
+            toc = time.perf_counter()
+            print(f"Solved {what} in {toc - tic:0.4f} seconds")   
+            return result
+        return wrapper
+    return decorator  
+
 def layout_tiles():
     tiles = {}
     lines = open('Day24/day24.txt').read().splitlines()
@@ -44,55 +58,58 @@ def layout_tiles():
             tiles[tile] = not tiles[tile]
     return set([tile for tile in tiles if tiles[tile]])
     
-
+@time_it("Part One")
 def part_one():
     tiles = layout_tiles()
     return len(tiles)
 
+def next_generation(x, y, z, tiles, newTiles, checked):
+    checked.add((x,y,z))
+
+    count = 0
+    if (x+1,y-1,z) in tiles:  #e
+        count+=1
+    if (x-1,y+1,z) in tiles:  #w
+        count+=1
+    if (x,y+1,z-1) in tiles:  #nw
+        count+=1
+    if (x+1,y,z-1) in tiles:  #ne
+        count+=1
+    if (x,y-1,z+1) in tiles:  #se
+        count+=1                    
+    if (x-1,y,z+1) in tiles:  #sw
+        count+=1    
+
+    isCurrentlyBlack = (x,y,z) in tiles
+
+    if isCurrentlyBlack and (count == 0 or count > 2):
+        pass
+    elif not isCurrentlyBlack and (count == 2):
+        newTiles.add((x,y,z))
+    elif isCurrentlyBlack:
+        newTiles.add((x,y,z))
+
+@time_it("Part Two")
 def part_two():
     tiles = layout_tiles()
 
-    for day in range(100):
-
-        minX = min([tile[0] for tile in tiles])
-        maxX = max([tile[0] for tile in tiles])
-        minY = min([tile[1] for tile in tiles])
-        maxY = max([tile[1] for tile in tiles])
-        minZ = min([tile[2] for tile in tiles])
-        maxZ = max([tile[2] for tile in tiles])
-
+    for _ in range(100):
         newTiles = set()
-
-        for x in range(minX-2,maxX+2):
-            for y in range(minY-2,maxY+2):
-                for z in range(minZ-2,maxZ+2):
-                    count = 0
-                    if (x+1,y-1,z) in tiles:  #e
-                        count+=1
-                    if (x-1,y+1,z) in tiles:  #w
-                        count+=1
-                    if (x,y+1,z-1) in tiles:  #nw
-                        count+=1
-                    if (x+1,y,z-1) in tiles:  #ne
-                        count+=1
-                    if (x,y-1,z+1) in tiles:  #se
-                        count+=1                    
-                    if (x-1,y,z+1) in tiles:  #sw
-                        count+=1    
-
-                    isCurrentlyBlack = (x,y,z) in tiles
-
-                    if isCurrentlyBlack and (count == 0 or count > 2):
-                        pass
-                    elif not isCurrentlyBlack and (count == 2):
-                        newTiles.add((x,y,z))
-                    elif isCurrentlyBlack:
-                        newTiles.add((x,y,z))
+        checked = set()
+        for blackTile in tiles:
+            (x,y,z) = blackTile
+            if not (x,y,z) in checked: next_generation(x,y,z, tiles, newTiles, checked)
+            if not (x+1,y-1,z) in checked: next_generation(x+1,y-1,z, tiles, newTiles, checked)
+            if not (x-1,y+1,z) in checked: next_generation(x-1,y+1,z, tiles, newTiles, checked)
+            if not (x,y+1,z-1) in checked: next_generation(x,y+1,z-1, tiles, newTiles, checked)
+            if not (x+1,y,z-1) in checked: next_generation(x+1,y,z-1, tiles, newTiles, checked)
+            if not (x,y-1,z+1) in checked: next_generation(x,y-1,z+1, tiles, newTiles, checked)
+            if not (x-1,y,z+1) in checked: next_generation(x-1,y,z+1, tiles, newTiles, checked)
         tiles = newTiles
-        print(day+1, len(tiles))
     return len(tiles)
 
 
-print(part_one())
-print(part_two())
+print(part_one())  #277 Solved Part One in 0.0117 seconds
+print(part_two())  #3531 Solved Part Two in 2.5060 seconds
 
+cProfile.run('part_two()')
